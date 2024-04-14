e4ce49aeaf8b6ceb531254baf0366b6ffe690331
shuhrat <keldiboyevshuhrat062@gmail.com>
2024-04-14 19:59:45 +0500

    siri new file push

---
+++ ranglar.js
@@ -0,0 +1,10 @@
+let body = document.querySelector("#div-row-copy11");
+
+function  function1(e){
+// manzil.style.background = `${e}`
+
+
+body.style.background = `${e}`
+
+navigator.clipboard.writeText(e)
+}
